b9913e9d563fdd8d696335f2cae944a355204777
HarshaDaparti <ux46295@umbc.edu>
2021-03-22 13:14:33 -0700

    init

--- main.tf
+++ main.tf
@@ -1,47 +1,34 @@
+provider "azurerm" {
+  version = "~> 1.34.0"
+  subscription_id = "${var.subscription_id}"
+  tenant_id = "${var.tenant_id}"
+}
 resource "azurerm_resource_group" "rg" {
-  name     = var.resource_group_name
-  location = var.resource_group_location
+  name = "${var.resource_group_name}"
+  location = "${var.resource_group_location}"
 }
-
-resource "random_integer" "ri" {
-  min = 10000
-  max = 99999
-}
-
-resource "azurerm_cosmosdb_account" "db" {
-  name                = "tfex-cosmos-db-${random_integer.ri.result}"
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
-  offer_type          = "Standard"
-  kind                = "GlobalDocumentDB"
-
+resource "azurerm_cosmosdb_account" "acc" {
+  name = "${var.cosmos_db_account_name}"
+  location = "${azurerm_resource_group.rg.location}"
+  resource_group_name = "${azurerm_resource_group.rg.name}"
+  offer_type = "Standard"
+  kind = "GlobalDocumentDB"
   enable_automatic_failover = true
-
-  capabilities {
-    name = "EnableAggregationPipeline"
+consistency_policy {
+    consistency_level = "Session"
   }
-
-  capabilities {
-    name = "mongoEnableDocLevelTTL"
-  }
-
-  capabilities {
-    name = "MongoDBv3.4"
-  }
-
-  consistency_policy {
-    consistency_level       = "BoundedStaleness"
-    max_interval_in_seconds = 10
-    max_staleness_prefix    = 200
-  }
-
+  
   geo_location {
-    location          = var.failover_location
+    location = "${var.failover_location}"
     failover_priority = 1
   }
-
-  geo_location {
-    location          = azurerm_resource_group.rg.location
+geo_location {
+    location = "${var.resource_group_location}"
     failover_priority = 0
   }
+}
+resource "azurerm_cosmosdb_sql_database" "db" {
+  name = "products"
+  resource_group_name = "${azurerm_cosmosdb_account.acc.resource_group_name}"
+  account_name = "${azurerm_cosmosdb_account.acc.name}"
 }
---
+++ variables.tf
@@ -0,0 +1,18 @@
+variable "subscription_id" {
+  default = "28fee576-30c3-491a-a826-37838759b6e4"
+}
+variable "tenant_id" {
+  default = "72f988bf-86f1-41af-91ab-2d7cd011db47"
+}
+variable "resource_group_name" {
+  default = "cosmoslabs-hd"
+}
+variable "resource_group_location" {
+  default = "West US"
+}
+variable "cosmos_db_account_name" {
+  default = "cosmoslab-tf-test"
+}
+variable "failover_location" {
+  default = "East US"
+}
